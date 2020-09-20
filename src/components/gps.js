ee6d2b0104ad5f807de1b083c7bbdf777fadb679
Kenson <kenson.nguyen3@gmail.com>
2020-09-20 11:49:26 -0700

    GPS label fix

--- src/components/gps.js
+++ src/components/gps.js
@@ -6,8 +6,8 @@ class Weather extends React.Component{
         return (
           <div className="GPS">
                <div className="card-content GPS">
-                    <p className="title is-6">Latitude: {this.props.gps.lat}</p>
-                    <p className="title is-6">Longitude: {this.props.gps.long}</p>
+                    <p className="title is-6">Latitude: {this.props.gps.latitude}</p>
+                    <p className="title is-6">Longitude: {this.props.gps.longitude}</p>
                </div>
           </div>
         );
