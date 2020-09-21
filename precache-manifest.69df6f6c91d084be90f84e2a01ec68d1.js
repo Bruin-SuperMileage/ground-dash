74b6c926036fe7b244bda41c327154694c2004d5
Kenson <kenson.nguyen3@gmail.com>
2020-09-21 02:52:05 -0700

    Updates

--- precache-manifest.69df6f6c91d084be90f84e2a01ec68d1.js
+++ precache-manifest.69df6f6c91d084be90f84e2a01ec68d1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "74066485a700d93699ec85df23da491a",
+    "revision": "98be1681ca3cc9ca3433be74d127c7ac",
     "url": "/ground-dash/index.html"
   },
   {
-    "revision": "4ddd63eeb1191f802d77",
-    "url": "/ground-dash/static/css/main.cec6413b.chunk.css"
+    "revision": "414569b1039d936c84db",
+    "url": "/ground-dash/static/css/main.429ce322.chunk.css"
   },
   {
     "revision": "e84b7b37a6bc5d235f5e",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ground-dash/static/js/2.689620af.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4ddd63eeb1191f802d77",
+    "revision": "414569b1039d936c84db",
     "url": "/ground-dash/static/js/main.b167e15f.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ground-dash/precache-manifest.5ea8bb2eaf37368898cdf6f1de644e6a.js"
+  "/ground-dash/precache-manifest.69df6f6c91d084be90f84e2a01ec68d1.js"
 );
 
 self.addEventListener('message', (event) => {
