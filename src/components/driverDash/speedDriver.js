5f16201dfb0532ba6bf81075fb8f51cb1c4d7781
Kenson <kenson.nguyen3@gmail.com>
2020-09-21 15:26:41 -0700

    MPH to m/s

--- src/components/driverDash/speedDriver.js
+++ src/components/driverDash/speedDriver.js
@@ -6,7 +6,7 @@ class SpeedDriver extends React.Component{
           <div className="columns">
                <div className="column">
                     <p className = "speed has-text-centered big">{this.props.speed.speed}</p>
-                    <p className = "speed has-text-centered km">km/h</p>
+                    <p className = "speed has-text-centered km">m/s</p>
                </div>
           </div>
         );
