8aa0d3df940fd5ff47429a2891a5831280ee99ca
KensonN <kenson.nguyen3@gmail.com>
2022-03-31 15:02:48 -0700

    altitude m to feet

--- src/components/groundDash/environment.js
+++ src/components/groundDash/environment.js
@@ -7,7 +7,7 @@ class Environment extends React.Component{
       <div className="battery">
          <div className="card-content">
           <p className="title is-5">Environment</p>
-          <p className="subtitle is-6">Altitude: {this.props.environment.altitude} m</p>
+          <p className="subtitle is-6">Altitude: {this.props.environment.altitude} ft</p>
           <p className="subtitle is-6">Temperature: {this.props.environment.temperature} °C</p>
          </div>
       </div>
